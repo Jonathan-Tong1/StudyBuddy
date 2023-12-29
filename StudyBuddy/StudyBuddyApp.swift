5204f0c4ce0e103af536381a9f13115d9c37d7cb
Jonathan Tong <jontong321@gmail.com>
2023-12-29 10:00:14 -0500

    Initial Commit

---
+++ StudyBuddy/StudyBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyBuddyApp.swift
+//  StudyBuddy
+//
+//  Created by Jonathan on 2023-12-29.
+//
+
+import SwiftUI
+
+@main
+struct StudyBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
