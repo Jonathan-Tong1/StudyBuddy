9844b7d4adb983d37ed55befe64977af27c8aa36
Jonathan Tong <jontong321@gmail.com>
2023-12-29 10:22:23 -0500

    querying SwiftData objects

--- StudyBuddy/ContentView.swift
+++ StudyBuddy/ContentView.swift
@@ -5,17 +5,44 @@
 //  Created by Jonathan on 2023-12-29.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    
+    @Query var assessments: [Assessment]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(assessments) { assessment in
+                    VStack(alignment: .leading) {
+                        Text(assessment.name)
+                            .font(.headline)
+                        
+                        Text(assessment.date.formatted(date: .long, time:
+                                .shortened))
+                        
+                    }
+                }
+            }
+            .navigationTitle("StudyBuddy")
+            .toolbar {
+                Button("Add Samples", action: addSamples)
+            }
         }
-        .padding()
+    }
+    
+    func addSamples() {
+        let exam1 = Assessment(name: "math135 exam")
+        let exam2 = Assessment(name: "math136 exam")
+        let exam3 = Assessment(name: "math138 exam")
+        
+        modelContext.insert(exam1)
+        modelContext.insert(exam2)
+        modelContext.insert(exam3)
+        
     }
 }
 
